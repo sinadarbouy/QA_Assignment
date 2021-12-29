844a1a991be3580c06cd010014f579ad625eb695
Johan Gottlander <johangottlander@gmail.com>
2021-12-29 16:24:01 +0100

    Added merge sort to group5test

--- a2-sorting-algorithms/src/test/java/murraco/Group5Test.java
+++ a2-sorting-algorithms/src/test/java/murraco/Group5Test.java
@@ -186,6 +186,48 @@ public class Group5Test {
         assertEquals("[2, 7, 10, 35]", Arrays.toString(data));
     }
 
+    @Test
+    public void testMergeSort() {
+        final Integer[] data = { 4, 3, 0, 11, 7, 5, 15, 12, 99, 1  };
+        MergeSort.mergeSort(data);
+        assertEquals("[0, 1, 3, 4, 5, 7, 11, 12, 15, 99]", Arrays.toString(data));
+    }
+
+    @Test
+    public void testMergeSortOneNumber() {
+        final Integer[] data = {4};
+        MergeSort.mergeSort(data);
+        assertEquals("[4]", Arrays.toString(data));
+    }
+
+    @Test
+    public void testMergeSortPositiveAndNegativeNumbers() {
+        final Integer[] data = { 4, -3, 0, -11, -7, 5, 15, 12, -99, 1 };
+        MergeSort.mergeSort(data);
+        assertEquals("[-99, -11, -7, -3, 0, 1, 4, 5, 12, 15]", Arrays.toString(data));
+    }
+
+    @Test
+    public void testMergeSortOrdered() {
+        final Integer[] data = { 1, 2, 3, 5, 7, 9, 10, 15, 20, 100 };
+        MergeSort.mergeSort(data);
+        assertEquals("[1, 2, 3, 5, 7, 9, 10, 15, 20, 100]", Arrays.toString(data));
+    }
+
+    @Test
+    public void testMergeSortOrdered2() {
+        final Integer[] data = { 100, 20, 15, 10, 9, 7, 5, 3, 2, 1};
+        MergeSort.mergeSort(data);
+        assertEquals("[1, 2, 3, 5, 7, 9, 10, 15, 20, 100]", Arrays.toString(data));
+    }
+
+    @Test
+    public void testMergeSortEmpty() {
+        final Integer[] data = {};
+        MergeSort.mergeSort(data);
+        assertEquals("[]", Arrays.toString(data));
+    }
+
 
     // causes error, not handled by the algorithm - E in BICEPS (error)
 /**    @Test
